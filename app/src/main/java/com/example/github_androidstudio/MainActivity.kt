1cef75e32a967be9e19afe3e9aa61ee464849966
haim1121 <haim112177@naver.com>
2021-08-11 01:53:45 +0900

    Long time no sleep

--- app/src/main/java/com/example/github_androidstudio/MainActivity.kt
+++ app/src/main/java/com/example/github_androidstudio/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.github_androidstudio.databinding.ActivityMainBinding
 import kotlin.concurrent.timer
 
@@ -61,9 +62,31 @@ class MainActivity : AppCompatActivity() {
             if(sec==0&&min==0&&hour==0){
                 return@setOnClickListener
             }
+
+            /*
+            binding.hText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+            binding.mText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+            binding.sText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+
+            binding.hour.setHintTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+            binding.min.setHintTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+            binding.second.setHintTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+
+            binding.cancelBtn.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+            binding.countdownText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+
+            binding.imgMiniLight.visibility=View.VISIBLE
+            binding.countdownButton.visibility=View.INVISIBLE
+
+            binding.imgBody1.setImageResource(R.drawable.on_light_body5)
+            binding.imgBodyEnd.setImageResource(R.drawable.on_light_body6)
+            */
+            // on 색으로 변경
+            color_change_on()
+            
             timer(period = 1000, initialDelay = 1000) {
                 runOnUiThread {
-                    binding.countdownText.text = String.format("%02d : %02d : %02d", hour, min, sec)
+                    binding.countdownText.text = String.format("%02d:%02d:%02d", hour, min, sec)
                 }
                 if (turnonalarm == true) {
                     if (isChecked) {
@@ -75,7 +98,8 @@ class MainActivity : AppCompatActivity() {
                     }
                     binding.cancelBtn.setOnClickListener {
                         torch.flashOff()
-                        binding.countdownText.text = String.format("%02d : %02d : %02d", 0,0,0)
+                        binding.countdownText.text = String.format("%02d:%02d:%02d", 0,0,0)
+                        color_change_off()
                         cancel()
                     }
                 }
@@ -95,7 +119,8 @@ class MainActivity : AppCompatActivity() {
                     }
                     sec--
                     binding.cancelBtn.setOnClickListener {
-                        binding.countdownText.text = String.format("%02d : %02d : %02d", 0,0,0)
+                        binding.countdownText.text = String.format("%02d:%02d:%02d", 0,0,0)
+                        color_change_off()
                         cancel()
                     }
                 }
@@ -133,6 +158,52 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, secondactivity::class.java)
 
         startActivity(intent)
+    }
+
+    // 켰을 때 색 바꾸기
+    fun color_change_on(){
+        //binding.hText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+        //binding.mText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+        //binding.sText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+        //binding.alarmSetText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+
+        binding.hour.setHintTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+        binding.min.setHintTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+        binding.second.setHintTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+
+        binding.cancelBtn.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+        binding.countdownText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_2))
+
+        binding.imgMiniLight.visibility=View.VISIBLE
+        binding.countdownButton.visibility=View.INVISIBLE
+        binding.cancelBtn.setText("cancel")
+        //binding.cancelBtn.visibility=View.VISIBLE
+
+        binding.imgBody1.setImageResource(R.drawable.on_light_body5)
+        binding.imgBodyEnd.setImageResource(R.drawable.on_light_body6)
+    }
+
+    // 껐을 때 색 바꾸기
+    fun color_change_off(){
+        //binding.hText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_1))
+        //binding.mText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_1))
+        //binding.sText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_1))
+        //binding.alarmSetText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_1))
+
+        binding.hour.setHintTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_4))
+        binding.min.setHintTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_4))
+        binding.second.setHintTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_4))
+
+        binding.cancelBtn.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_4))
+        binding.countdownText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_3))
+
+        binding.imgMiniLight.visibility=View.INVISIBLE
+        binding.countdownButton.visibility=View.VISIBLE
+        binding.cancelBtn.setText("reset")
+        //binding.cancelBtn.visibility=View.INVISIBLE
+
+        binding.imgBody1.setImageResource(R.drawable.off_light_body5)
+        binding.imgBodyEnd.setImageResource(R.drawable.off_light_body6)
     }
 }
 class ActivityMainBinding {
--- app/src/main/java/com/example/github_androidstudio/secondactivity.kt
+++ app/src/main/java/com/example/github_androidstudio/secondactivity.kt
@@ -2,7 +2,9 @@ package com.example.github_androidstudio
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.github_androidstudio.databinding.ActivityMainBinding
 import kotlin.concurrent.timer
 
@@ -26,9 +28,36 @@ class secondactivity : AppCompatActivity() {
             }
             binding.cancelBtn.setOnClickListener{
                 Log.d(TAG,"secondactivity - onBackButtonClicked() called")
+                // Main에서 이미지 바꾸기
+                color_change_off()
                 finish()
                 cancel()
+
             }
         }
     }
+
+    // 껐을때, MainActivity에도 존재함.
+    fun color_change_off(){
+        //binding.hText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_1))
+        //binding.mText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_1))
+        //binding.sText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_1))
+        //binding.alarmSetText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_1))
+
+        binding.hour.setHintTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_4))
+        binding.min.setHintTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_4))
+        binding.second.setHintTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_4))
+
+        binding.cancelBtn.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_4))
+        binding.countdownText.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.main_3))
+
+        binding.imgMiniLight.visibility= View.INVISIBLE
+        binding.countdownButton.visibility= View.VISIBLE
+        binding.cancelBtn.setText("reset")
+        //binding.cancelBtn.visibility=View.INVISIBLE
+
+        binding.imgBody1.setImageResource(R.drawable.off_light_body5)
+        binding.imgBodyEnd.setImageResource(R.drawable.off_light_body6)
+    }
 }
+
